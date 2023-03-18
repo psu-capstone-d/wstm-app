26b7281bfd68f47a729d8251b6d3db9eab9a30ef
devonfox <devonhouston.fox@gmail.com>
2023-03-17 18:58:56 -0700

    added results page ~ placeholder for now

--- src/fixtures/fixtures.tsx
+++ src/fixtures/fixtures.tsx
@@ -3,8 +3,9 @@ import {module1} from 'src/fixtures/module1'
 import {module2} from 'src/fixtures/module2'
 import {module3} from 'src/fixtures/module3'
 import {module4} from 'src/fixtures/module4'
+import {result} from 'src/fixtures/result'
 
-const modules: ModuleFixtures[] = [module1, module2, module3, module4]
+const modules: ModuleFixtures[] = [module1, module2, module3, module4, result]
 
 const idGenerator = () => {
   let currentId = 0
@@ -31,7 +32,7 @@ const titleGenerator = (moduleNum: number) => {
 const [questionTitleGenerator] = titleGenerator(modules.length)
 
 export const demoCourse: Course = {
-  name: 'Demo Course',
+  name: 'WSTM Modules',
   modules: modules.map<Module>(({title, activities}, i) => ({
     id: moduleIdGenerator(),
     title: title,
---
+++ src/fixtures/result.tsx
@@ -0,0 +1,15 @@
+import {ModuleFixtures} from 'src/types'
+
+// Dummy Activity so I can display
+export const result: ModuleFixtures = {
+  title: '',
+  activities: [
+    {
+      type: 'text',
+      title: '',
+      text:
+        '🎉🎉🎉\nScore'
+    },
+    
+  ],
+}
